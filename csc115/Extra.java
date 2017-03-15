cda274f4288c162498038059f56111d10f29a193
paulcheng96 <paulcheng96@gmail.com>
2017-03-14 21:22:34 -0700

    Add files via upload

--- csc115/Extra.java
+++ csc115/Extra.java
@@ -1,47 +1,51 @@
-import java.util.*;
-public class Extra{
-	ArrayList<String> list;
+public class Extra {
+	private BSTRefBased t;
 	
-	public Extra() {
-        list = new ArrayList<String>();
-    }
-	
-	//Extra method for print the StringList
-	public void PrintPostfix(StringList expr){
-		for(int i=0;i<expr.getLength();i++){
-			System.out.println(expr.retrieve(i));
+	//This method will return how many node in the current tree
+	public int GetNodeNum(TreeNode node){
+		if(node==null){
+			return 0;
 		}
+		return GetNodeNum(node.left) + GetNodeNum(node.right) + 1;
 	}
 	
-	public void insertTail(String item) {
-        list.add(item);
-    }
-	
-	//Extra method to search elements in the ArrayList
-	public boolean SearchElements(String expr){
-		if (list.size() == 0) {
-            return false;
-		}else if(list.contains(expr)){
-			return true;
-		} else{
-			return false;
+	//This method will return the height of the current tree
+	public int GetDepth(TreeNode node){
+		if(node==null){
+			return 0;
 		}
-		
+		int depthleft = GetDepth(node.left);
+		int depthright = GetDepth(node.right);
+		return depthleft > depthright ? (depthleft + 1) : (depthright + 1);  
 	}
 	
-	public static void main(String[] args) {
-        Extra t1 = new Extra();
-		boolean    passed;
-        StringList expr = new StringList();
+	public static void main(String[] args){
+		BSTRefBased t;
+		Extra tt = new Extra();
+		boolean result;
+		String message;
 		
-		String[] a1 = {"aaa", "bbb", "ccc", "ddd"};
-        for (String s : a1) {
-            t1.insertTail(s);
-			expr.insertTail(s);
-        }		
-		if (t1.SearchElements("aaa")==true){
-			System.out.println("Test1 Passed");
-		}		
-		t1.PrintPostfix(expr);
-}
+		message = "Test 1: inserting 'humpty' -- ";
+        t = new BSTRefBased();
+        try {
+            t.insert("humpty");
+            result = tt.GetNodeNum(t.getRoot())==1;
+			result &=tt.GetDepth(t.getRoot())==1;
+        } catch (Exception e) {
+            result = false;
+        }
+        System.out.println(message + (result ? "passed" : "FAILED"));
+		
+		message = "Test 2: inserting 'humpty' and 'dumpty' -- ";
+        t = new BSTRefBased();
+        try {
+            t.insert("humpty");
+			t.insert("dumpty");
+            result = tt.GetNodeNum(t.getRoot())==2;
+			result &=tt.GetDepth(t.getRoot())==2;
+        } catch (Exception e) {
+            result = false;
+        }
+        System.out.println(message + (result ? "passed" : "FAILED"));
+	}
 }
